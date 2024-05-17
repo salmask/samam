74e102865a0d699ce9238ab04a80a6aca4870b36
Abdul Khuddus <khuddus1@gmail.com>
2024-05-18 00:07:15 +0530

    feat: updated index meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,22 @@ import Header from '~/components/Layout/Header';
 import Footer from '~/components/Layout/Footer';
 import News from '~/components/News/News';
 import UpcomingEvents from '~/components/Events/UpcomingEvents';
-
+import { MetaFunction } from '@remix-run/node';
+export const meta: MetaFunction = () => {
+  return [{
+      title: "Satyanveshana Mandali - Path to wisdom",
+      description: "Satyanveshana Mandali is your go-to platform for exploring knowledge, events, publications, and more. Stay updated with the latest happenings and enrich your knowledge.",
+      keywords: "Satyanveshana Mandali, philosophy, epistemology, knowledge, events, publications, contacts",
+      "og:title": "Satyanveshana Mandali",
+      "og:description": "Explore knowledge and events with Satyanveshana Mandali. Stay updated with our latest publications and contacts.",
+      "og:image": "/assets/logo.png",  // Make sure this path is correct
+      "og:url": "https://www.satyanveshanamandali.com",  // Replace with your actual URL
+      "twitter:card": "summary_large_image",
+      "twitter:title": "Satyanveshana Mandali - Explore Knowledge and Events",
+      "twitter:description": "Satyanveshana Mandali is your go-to platform for exploring knowledge, philosophy, epistemology, events, publications, and more. Stay updated with the latest happenings and enrich your knowledge.",
+      "twitter:image": "/assets/logo.png"  // Make sure this path is correct
+  }]
+};
 export default function Index() {
   return (
     <div className="container mx-auto p-4">
